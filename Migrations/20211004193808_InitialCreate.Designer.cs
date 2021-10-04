3e7575942acd74dc3521c44e1a1c4464da171364
bpesquet <bpesquet@gmail.com>
2021-10-04 21:54:51 +0200

    Add a new field: drop and re-create database

--- Migrations/20211004193808_InitialCreate.Designer.cs
+++ Migrations/20211004193808_InitialCreate.Designer.cs
@@ -9,7 +9,7 @@ using MvcMovie.Data;
 namespace MvcMovie.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    [Migration("20211004145408_InitialCreate")]
+    [Migration("20211004193808_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -28,6 +28,9 @@ namespace MvcMovie.Migrations
                         .HasColumnType("TEXT");
 
                     b.Property<decimal>("Price")
+                        .HasColumnType("decimal(18, 2)");
+
+                    b.Property<string>("Rating")
                         .HasColumnType("TEXT");
 
                     b.Property<DateTime>("ReleaseDate")
